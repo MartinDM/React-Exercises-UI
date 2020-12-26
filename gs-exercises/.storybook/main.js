cc2484fb6d4f87246eabc343c4215ec9d66c416f
martinDM <hello@martindm.uk>
2020-12-26 19:18:06 +0000

    Initial commitq

---
+++ gs-exercises/.storybook/main.js
@@ -0,0 +1,11 @@
+// .storybook/main.js
+
+module.exports = {
+  stories: ['../src/components/**/*.stories.js'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-storyshots',
+    '@storybook/preset-create-react-app',
+  ],
+};
